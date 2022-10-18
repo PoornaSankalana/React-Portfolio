1ea6a3856a52ebd20f221d43f026d8d0754b2c57
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-18 13:52:45 +0530

    changes done

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,54 @@
-import React, { Component } from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
+
 export default class About extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <section id="about">
-         <div className="row">
+	render() {
+		let resumeData = this.props.resumeData;
 
-            <div className="three columns">
-
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
-
-            </div>
-
-            <div className="nine columns main-col">
-
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.website}</span>
-       					   </p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-    );
-  }
+		return (
+			<section id="about">
+				<div className="row">
+					<div className="three columns">
+						<img
+							className="profile-pic"
+							src="images/profilepic.jpg"
+							alt=""
+						/>
+					</div>
+					<div className="nine columns main-col">
+						<h2> About Me </h2> <p> {resumeData.aboutme} </p>
+						<div className="row">
+							<div className="columns contact-details">
+								<h2> Contact Details </h2>{" "}
+								<p className="address">
+									<span> {resumeData.name} </span> <br />
+									<span>
+										{" "}
+										{resumeData.address}{" "}
+									</span>{" "}
+									<br />
+									<span> {resumeData.email} </span>{" "}
+									<br />
+									<span> {resumeData.mobile} </span>{" "}
+									<br />
+									<br />
+									<span>
+										{" "}
+										<a
+											id="link"
+											className="cv"
+											href="documents/CV.pdf"
+										>
+											Download CV
+										</a>{" "}
+									</span>{" "}
+								</p>{" "}
+							</div>{" "}
+						</div>{" "}
+					</div>{" "}
+				</div>{" "}
+			</section>
+		);
+	}
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,127 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
+    imagebaseurl: "https://rbhatia46.github.io/",
+    name: "Poorna Sankalana",
+    role: "Full Stack Web and Mobile Application Developer",
+    linkedinId: "Poorna Sankalana",
+    facebookId: "Poorna Sankalana",
+    githubId: "PoornaSankalana",
+    email: "poorna.skl22@gmail.com",
+    tele: "070 243 1416",
+    roleDescription: "I like dabbling in various parts of frontend and backend development and like to learn about new technologies or simply play games in my free time.",
+    socialLinks: [{
+            name: "facebook",
+            url: "https://www.facebook.com/poorna.sankalana.3",
+            className: "fa fa-facebook",
         },
         {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
+            name: "github",
+            url: "https://github.com/PoornaSankalana",
+            className: "fa fa-github",
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/poorna-sankalana-67213620b/",
+            className: "fa fa-linkedin",
+        },
     ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
+    aboutme: "I am currently a pre-final year student at Sri Lankan Institute of Information Technology and pursuing my BSc. from here. I am a self taught Full Stack Web Developer, currently diving deeper into Mobile Application Development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    address: "Okkampitiya Road, Medagama, Buttala. Sri Lanka",
+    mobile: "070 243 1416",
+    education: [{
+            UniversityName: "British Council",
+            specialization: "Certificate Course in Spoken English",
+            MonthOfPassing: "Completed in 2019",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
+        {
+            UniversityName: "Sri Lankan Institute of Information Technology",
+            specialization: "Specialization in Software Engineering",
+            MonthOfPassing: "Still Reading From 2020",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
+        {
+            UniversityName: "University Of Moratuwa",
+            specialization: "Trainee Full Stack Developer",
+            MonthOfPassing: "Still Reading From 2020",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
     ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
+    work: [{
+        CompanyName: "Antler IT Solutions",
+        specialization: "Intern - Software Engineer",
+        MonthOfLeaving: "Still Working From 2022 September",
+        // YearOfLeaving: "2018",
+        // Achievements: "Some Achievements",
+    }, ],
+    skillsDescription: "Your skills here",
+    skills: [{
+            skillname: "HTML5",
+        },
+        {
+            skillname: "CSS",
+        },
+        {
+            skillname: "React",
+        },
+        {
+            skillname: "Node",
+        },
+        {
+            skillname: "Photoshop",
+        },
+        {
+            skillname: "Illustrator",
+        },
+        {
+            skillname: "Javascript",
+        },
+        {
+            skillname: "Python",
+        },
+        {
+            skillname: "Java",
+        },
+        {
+            skillname: "SQL",
+        },
+        {
+            skillname: "MongoDB",
+        },
+        {
+            skillname: "C",
+        },
     ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
+    portfolio: [{
+            name: "CareForYou Laboratories",
+            description: "Web Application",
+            imgurl: "images/portfolio/CareForU.png",
+            giturl: "https://github.com/PoornaSankalana/Online-Laboratory-Management-System-Frontend",
+        },
+        {
+            name: "BrainTech Institute",
+            description: "Web Application",
+            imgurl: "images/portfolio/BraintechInstitute.png",
+            giturl: "https://github.com/PoornaSankalana/ITP-frontEnd",
+        },
+        {
+            name: "FashionHub Online Fashion Store",
+            description: "Mobile Application",
+            imgurl: "images/portfolio/FashionHub.png",
+            giturl: "https://github.com/PoornaSankalana/ITP-frontEnd",
+        },
     ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+    testimonials: [{
+            description: "Truely Recommend this guy for your project development.",
+            name: "Trainee Software Engineer - Surge Global - Sasindu Nanayakkara",
+        },
+        {
+            description: "Truely Recommend this guy for your project development.",
+            name: "Assiciate Software Engineer - Arimac - Susith Rupasinghe",
+        },
+    ],
+};
+
+export default resumeData;
